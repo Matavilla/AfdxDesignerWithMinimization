9c9e816d7746c3533d626362bebfa7c1fcfe5cf9
Matavilla <mr.andrej1102@yandex.ru>
2020-03-23 17:03:14 +0300

    add first test. need to prove count length

--- generate_tests/generator_high_data.py
+++ generate_tests/generator_high_data.py
@@ -5,7 +5,7 @@ import os, subprocess
 import sys
 
 dir_name = "tests_high_data"
-number_of_tests = 100
+number_of_tests = 1
 if len(sys.argv) == 2:
     number_of_tests = int(sys.argv[1])
 number_of_msgs = 100
@@ -16,10 +16,10 @@ periodMax = 10000
 tMaxMin = 1000
 tMaxMax = 10000
 destNumMin = 1
-destNumMax = 1
+destNumMax = 4
 
-numberOfGroups = 8
-partGroups = [[9, 10], [11,12], [13,14,15], [16], [17,18], [19, 20], [21,22], [23,24]]
+numberOfGroups = 18
+partGroups = [[28, 29, 30], [31, 32, 33], [34], [35], [36, 37, 38], [39, 40, 41], [42], [43], [44, 45, 46], [47, 48, 49], [50, 51, 52], [53, 54, 55],[ 56, 57, 58], [59, 60, 61], [62, 63], [64, 65], [66, 67, 68], [69, 70, 71]]
 
 def generate_one_df(fromPartition, toPartitions, jMax, msgSize, period, tMax, id):
     dest = ""
@@ -72,11 +72,14 @@ def generateOneTest():
     return text
 
 num_of_requests = number_of_msgs
-num_of_assigned = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'without_source': 0, 'without_resp': 0, 'without_lim': 0, 'without_redesign': 0}
-num_of_vls = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'without_source': 0, 'without_resp': 0, 'without_lim': 0, 'without_redesign': 0}
-av_time = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'without_source': 0, 'without_resp': 0, 'without_lim': 0, 'without_redesign': 0}
 
-configs = {'lim_1': '--limited-search-depth=1', 'lim_2': '--limited-search-depth=2', 'lim_3': '--limited-search-depth=3', 'without_source': '--disable-aggr-on-source', 'without_resp': '--disable-aggr-on-resp-time', 'without_lim': '--disable-limited-search', 'without_redesign': '--disable-redesign'}
+num_of_assigned = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'c1 = 0.0 c2 = 1.0': 0, 'c1 = 0.1 c2 = 0.9': 0, 'c1 = 0.2 c2 = 0.8': 0, 'c1 = 0.3 c2 = 0.7': 0, 'c1 = 0.4 c2 = 0.6': 0, 'c1 = 0.5 c2 = 0.5': 0, 'c1 = 0.6 c2 = 0.4': 0, 'c1 = 0.7 c2 = 0.3': 0, 'c1 = 0.8 c2 = 0.2': 0, 'c1 = 0.9 c2 = 0.1': 0, 'c1 = 1.0 c2 = 0.0': 0}
+
+num_len = {'lim_1': 0.0, 'lim_2': 0.0, 'lim_3': 0.0, 'c1 = 0.0 c2 = 1.0': 0.0, 'c1 = 0.1 c2 = 0.9': 0.0, 'c1 = 0.2 c2 = 0.8': 0.0, 'c1 = 0.3 c2 = 0.7': 0.0, 'c1 = 0.4 c2 = 0.6': 0.0, 'c1 = 0.5 c2 = 0.5': 0.0, 'c1 = 0.6 c2 = 0.4': 0.0, 'c1 = 0.7 c2 = 0.3': 0.0, 'c1 = 0.8 c2 = 0.2': 0.0, 'c1 = 0.9 c2 = 0.1': 0.0, 'c1 = 1.0 c2 = 0.0': 0.0}
+
+num_of_vls = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'c1 = 0.0 c2 = 1.0': 0, 'c1 = 0.1 c2 = 0.9': 0, 'c1 = 0.2 c2 = 0.8': 0, 'c1 = 0.3 c2 = 0.7': 0, 'c1 = 0.4 c2 = 0.6': 0, 'c1 = 0.5 c2 = 0.5': 0, 'c1 = 0.6 c2 = 0.4': 0, 'c1 = 0.7 c2 = 0.3': 0, 'c1 = 0.8 c2 = 0.2': 0, 'c1 = 0.9 c2 = 0.1': 0, 'c1 = 1.0 c2 = 0.0': 0}
+
+configs = {'lim_1': '--limited-search-depth=1', 'lim_2': '--limited-search-depth=2', 'lim_3': '--limited-search-depth=3', 'c1 = 0.0 c2 = 1.0': '--coefficient-c1=0.0 --coefficient-c2=1.0', 'c1 = 0.1 c2 = 0.9': '--coefficient-c1=0.1 --coefficient-c2=0.9', 'c1 = 0.2 c2 = 0.8': '--coefficient-c1=0.2 --coefficient-c2=0.8', 'c1 = 0.3 c2 = 0.7': '--coefficient-c1=0.3 --coefficient-c2=0.7', 'c1 = 0.4 c2 = 0.6': '--coefficient-c1=0.4 --coefficient-c2=0.6', 'c1 = 0.5 c2 = 0.5': '--coefficient-c1=0.5 --coefficient-c2=0.5', 'c1 = 0.6 c2 = 0.4': '--coefficient-c1=0.6 --coefficient-c2=0.4', 'c1 = 0.7 c2 = 0.3': '--coefficient-c1=0.7 --coefficient-c2=0.3', 'c1 = 0.8 c2 = 0.2': '--coefficient-c1=0.8 --coefficient-c2=0.2', 'c1 = 0.9 c2 = 0.1': '--coefficient-c1=0.9 --coefficient-c2=0.1', 'c1 = 1.0 c2 = 0.0': '--coefficient-c1=1.0 --coefficient-c2=1.0'}
 
 def calc_num_of_assigned():
     process = subprocess.Popen(['grep', 'vl="None"', "out.afdxxml", "-c"], stdout=subprocess.PIPE)
@@ -89,11 +92,17 @@ def calc_num_of_vls():
     print output
     return output
 
+def calc_length():
+    process = subprocess.Popen(['grep', 'length: ', "out.afdxxml", "-c"], stdout=subprocess.PIPE)
+    output = float(process.communicate()[0])
+    print output
+    return output
+
 def generate_and_run():
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-    for i in range(1, number_of_tests+1):
+    for i in range(1, number_of_tests + 1):
         fileName = dir_name + '/' + 'test_'+ str(i) +'.afdxxml'
         f = open(fileName, 'w')
         f.write(generateOneTest())
@@ -107,28 +116,17 @@ def generate_and_run():
         for config in configs.keys():
             process = subprocess.Popen('time ' + name + ' ' + fileName + ' out.afdxxml ' + " a " + configs[config],  stdout=subprocess.PIPE,  stderr=subprocess.PIPE, shell=True)
             print config + " for test " + str(i) + " done"
-            output = process.communicate()
-            time = output[1].split()[3]
-
-            try:
-                minutes = float(time.split('m')[0])
-                secs = float(time.split('m')[1][:-1])
-            except:
-                print "exception!"
-                print time
-                return;
-            time = minutes*60 + secs
-            av_time[config] += time
 
             num_of_assigned[config] += calc_num_of_assigned()
             num_of_vls[config] += calc_num_of_vls()
+            num_len[config] += calc_length()
 
     for config in configs.keys():
         ans = config
         ans += '\t' + str(num_of_requests)
         ans += '\t' + str(int(float(num_of_assigned[config]) / number_of_tests))
         ans += '\t' + str(int(float(num_of_vls[config]) / number_of_tests))
-        ans += '\t' + str(av_time[config]/number_of_tests)
+        ans += '\t' + str(num_len[config] / number_of_tests)
         print ans
 
 generate_and_run()
