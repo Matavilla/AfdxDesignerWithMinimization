235e9a7bc5bae3c149dd74bf275ecafd9a8ad883
Matavilla <mr.andrej1102@yandex.ru>
2020-03-31 15:09:50 +0300

    BLYA 1 test is ready

--- generate_tests/generator_high_data.py
+++ generate_tests/generator_high_data.py
@@ -5,10 +5,10 @@ import os, subprocess
 import sys
 
 dir_name = "tests_high_data"
-number_of_tests = 1
+number_of_tests = 100
 if len(sys.argv) == 2:
     number_of_tests = int(sys.argv[1])
-number_of_msgs = 100
+number_of_msgs = 90
 msgSizeMin = 1000
 msgSizeMax = 1000000
 periodMin = 1000
@@ -16,7 +16,7 @@ periodMax = 10000
 tMaxMin = 1000
 tMaxMax = 10000
 destNumMin = 1
-destNumMax = 4
+destNumMax = 1
 
 numberOfGroups = 18
 partGroups = [[28, 29, 30], [31, 32, 33], [34], [35], [36, 37, 38], [39, 40, 41], [42], [43], [44, 45, 46], [47, 48, 49], [50, 51, 52], [53, 54, 55],[ 56, 57, 58], [59, 60, 61], [62, 63], [64, 65], [66, 67, 68], [69, 70, 71]]
@@ -81,21 +81,19 @@ num_of_vls = {'lim_1': 0, 'lim_2': 0, 'lim_3': 0, 'c1 = 0.0 c2 = 1.0': 0, 'c1 = 
 
 configs = {'lim_1': '--limited-search-depth=1', 'lim_2': '--limited-search-depth=2', 'lim_3': '--limited-search-depth=3', 'c1 = 0.0 c2 = 1.0': '--coefficient-c1=0.0 --coefficient-c2=1.0', 'c1 = 0.1 c2 = 0.9': '--coefficient-c1=0.1 --coefficient-c2=0.9', 'c1 = 0.2 c2 = 0.8': '--coefficient-c1=0.2 --coefficient-c2=0.8', 'c1 = 0.3 c2 = 0.7': '--coefficient-c1=0.3 --coefficient-c2=0.7', 'c1 = 0.4 c2 = 0.6': '--coefficient-c1=0.4 --coefficient-c2=0.6', 'c1 = 0.5 c2 = 0.5': '--coefficient-c1=0.5 --coefficient-c2=0.5', 'c1 = 0.6 c2 = 0.4': '--coefficient-c1=0.6 --coefficient-c2=0.4', 'c1 = 0.7 c2 = 0.3': '--coefficient-c1=0.7 --coefficient-c2=0.3', 'c1 = 0.8 c2 = 0.2': '--coefficient-c1=0.8 --coefficient-c2=0.2', 'c1 = 0.9 c2 = 0.1': '--coefficient-c1=0.9 --coefficient-c2=0.1', 'c1 = 1.0 c2 = 0.0': '--coefficient-c1=1.0 --coefficient-c2=1.0'}
 
-def calc_num_of_assigned():
-    process = subprocess.Popen(['grep', 'vl="None"', "out.afdxxml", "-c"], stdout=subprocess.PIPE)
+def calc_num_of_assigned(name):
+    process = subprocess.Popen(['grep', 'vl="None"', name, "-c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = int(process.communicate()[0])
     return num_of_requests - output
 
-def calc_num_of_vls():
-    process = subprocess.Popen(['grep', '<virtualLink ', "out.afdxxml", "-c"], stdout=subprocess.PIPE)
+def calc_num_of_vls(name):
+    process = subprocess.Popen(['grep', '<virtualLink ', name, "-c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = int(process.communicate()[0])
-    print output
     return output
 
-def calc_length():
-    process = subprocess.Popen(['grep', 'length: ', "out.afdxxml", "-c"], stdout=subprocess.PIPE)
+def calc_length(name):
+    process = subprocess.Popen(['grep', 'Sum length link: ', name, "-c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = float(process.communicate()[0])
-    print output
     return output
 
 def generate_and_run():
@@ -103,24 +101,43 @@ def generate_and_run():
         os.makedirs(dir_name)
 
     for i in range(1, number_of_tests + 1):
-        fileName = dir_name + '/' + 'test_'+ str(i) +'.afdxxml'
-        f = open(fileName, 'w')
-        f.write(generateOneTest())
-        f.close()
+        flag = True;
+        while flag:
+            os.system("rm -rf tests_high_data/*")
+            fileName = dir_name + '/' + 'test_'+ str(i) +'.afdxxml'
+            f = open(fileName, 'w')
+            f.write(generateOneTest())
+            f.close()
 
-        if sys.platform.startswith("win"):
-            name = "../algo/AFDX_DESIGN.exe"
-        else:
-            name = "../algo/AFDX_DESIGN"
+            if sys.platform.startswith("win"):
+                name = "../algo/AFDX_DESIGN.exe"
+            else:
+                name = "../algo/AFDX_DESIGN"
 
+            flag = False
+            j = 0
+            for config in configs.keys():
+                process = subprocess.Popen(name + ' ' + fileName + ' ' +  dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml ' + " a " + configs[config], stdout=subprocess.PIPE,  stderr=subprocess.PIPE, shell=True)
+                print config + " for test " + str(i) + " done"
+                print name + ' ' + fileName + ' ' +  dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml ' + " a " + configs[config]
+                process.wait()
+
+                try:
+                    calc_num_of_assigned(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+                    calc_num_of_vls(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+                    calc_length(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+                except:
+                    flag = True;
+                j += 1
+        j = 0
         for config in configs.keys():
-            process = subprocess.Popen(name + ' ' + fileName + ' out.afdxxml ' + " a " + configs[config],  stdout=subprocess.PIPE,  stderr=subprocess.PIPE, shell=True)
-            print config + " for test " + str(i) + " done"
+            num_of_assigned[config] += calc_num_of_assigned(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+            num_of_vls[config] += calc_num_of_vls(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+            num_len[config] += calc_length(dir_name + '/test_out' + str(i) + '_' + str(j) + '.afdxxml')
+            j += 1
+        print str(i) + ' done'
 
-            num_of_assigned[config] += calc_num_of_assigned()
-            num_of_vls[config] += calc_num_of_vls()
-            num_len[config] += calc_length()
-
+    print 
     for config in configs.keys():
         ans = config
         ans += '\t' + str(num_of_requests)
